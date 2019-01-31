479cd3b48a5174359545da89831a65f003c229b0
sirrom2005@gmail.com <tyrone2009>
2019-01-30 19:40:11 -0800

    Add project files.

---
+++ SMSTool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace SMSTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SMSTimer T = new SMSTimer();
+            T.Start();
+            Console.Read();
+            T.Stop();
+        }
+    }
+}
---
+++ SMSTool/WebClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SMSTool
+{
+    public class WebClient
+    {
+        public static async Task<int> PostDataAsync(string json)
+        {
+            HttpClient client = new HttpClient();
+            var values = new Dictionary<string, string>
+            {
+               { "json", json }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+            var response = await client.PostAsync(Config.API_URL, content);
+            var tmp = await response.Content.ReadAsStringAsync();
+            return int.Parse(tmp);
+        }
+    }
+}
